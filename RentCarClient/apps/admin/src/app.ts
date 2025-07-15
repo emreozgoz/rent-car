b769cea5202a5f1d6544988ee07b9bd084d31c09
Emre <emreozgoz@gmail.com>
2025-07-15 11:13:42 +0300

    fix

--- RentCarClient/apps/admin/src/app.ts
+++ RentCarClient/apps/admin/src/app.ts
@@ -4,7 +4,7 @@ import { RouterModule } from '@angular/router';
 @Component({
   imports: [RouterModule],
   selector: 'app-root',
-  templateUrl: '<router-outlet>',
+  template: '<router-outlet />',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class App {}
